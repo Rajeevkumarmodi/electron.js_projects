c6d177c4b0862bfdc9b2f6699c6662359259499f
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2024-02-04 09:42:47 +0530

    app setup and adding form

---
+++ db_crud_app/src/app.js
@@ -0,0 +1,17 @@
+const firstName = document.getElementById("firstName").value;
+const lastName = document.getElementById("lastName").value;
+const phone = document.getElementById("phone").value;
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const address = document.getElementById("address").value;
+const country = document.getElementById("country").value;
+const postalZipCode = document.getElementById("postalZipCode").value;
+const companyName = document.getElementById("companyName").value;
+const publishStatus = document.getElementById("publishStatus").value;
+const bankDetail = document.getElementById("postalZipCode").value;
+const userTable = document.getElementById("userTable");
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", () => {
+  console.log("clicked");
+});
